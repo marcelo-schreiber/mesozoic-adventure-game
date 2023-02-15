34b12e3727936ef7e12de93853e6e058c1c3bccd
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-02-15 02:30:13 -0300

    Add: consumable pellets and improved drawboard.

--- drawboard.py
+++ drawboard.py
@@ -1,6 +1,10 @@
 import pygame
 from settings import TILE_SIZE
 
+PATH = 0
+WALL = 1
+INVINCIBLE = 2
+ZAWARUDO = 3
 
 class CollideTile(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, color):
@@ -13,14 +17,14 @@ class CollideTile(pygame.sprite.Sprite):
 
 
 class PowerUpTile(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, x, y, width, height, color, type):
         super().__init__()
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.type_of_powerup = 'attack'
+        self.type_of_powerup = type
 
 
 class NonCollideTiles(pygame.sprite.Sprite):
@@ -43,14 +47,18 @@ def draw_board(level):
     # loop through level array
     for row in range(len(level)):
         for col in range(len(level[row])):
-            if level[row][col] == 1:
+            if level[row][col] == WALL:
                 collide_tiles.add(CollideTile(
                     col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE, 'black'))
-            elif level[row][col] == 0:
-                noncollide_tiles.add(NonCollideTiles(
-                    col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE, 'white'))
-            elif level[row][col] == 2:
+                continue
+            elif level[row][col] == PATH:
                 powerup_tiles.add(PowerUpTile(
-                    col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE, 'green'))
+                    col * TILE_SIZE + TILE_SIZE/4, row * TILE_SIZE + TILE_SIZE/4, TILE_SIZE/2, TILE_SIZE/2, 'green', 'scooby_snack'))
+            elif level[row][col] == INVINCIBLE:
+                powerup_tiles.add(PowerUpTile(
+                    col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE, 'blue', 'invinc'))
+
+            noncollide_tiles.add(NonCollideTiles(
+                    col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE, 'white'))
 
     return collide_tiles, noncollide_tiles, powerup_tiles
